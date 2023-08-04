13eed8c37c06c6d2371d940380445ea1163edf65
subhaganesh <subhaganesh5@outlook.com>
2023-08-05 01:05:43 +0530

    'update'

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,34 +5,45 @@ import altair as alt
 from altair.vegalite.v4.api import Chart
 
 
-
-
 #loading the saved model
 diabetes_model = pickle.load(open("diabetes_model.sav", 'rb'))
 
-
+scaler = pickle.load(open("scaler.sav", 'rb'))
 
 # creating a function for Prediction
 
 def diabetes_prediction(input_data):
-    # Changing the input_data to numpy array
+    
+
+    # changing the input_data to numpy array
     input_data_as_numpy_array = np.asarray(input_data)
 
     # reshape the array as we are predicting for one instance
     input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
 
-    prediction = diabetes_model.predict(input_data_reshaped)
+    scaled_input = scaler.transform(input_data_reshaped)
+
+    prediction = diabetes_model.predict(scaled_input)
     print(prediction)
 
-    if prediction[0] == 0:
-        return 'The person is not diabetic'
+    if (prediction[0] == 0):
+      return 'The person is not diabetic'
     else:
       return 'The person is diabetic'
-    
+
 def main():
     # Giving a title
     st.title('Diabetes Prediction Web App')
     
+    # Getting the input data from the user
+    Pregnancies = st.number_input('Number of Pregnancies', min_value=0, max_value=20, step=1)
+    Glucose = st.number_input('Glucose Level', min_value=0, max_value=500, step=1)
+    BloodPressure = st.number_input('Blood Pressure value', min_value=0, max_value=200, step=1)
+    SkinThickness = st.number_input('Skin Thickness value', min_value=0, max_value=100, step=1)
+    Insulin = st.number_input('Insulin Level', min_value=0, max_value=1000, step=1)
+    BMI = st.number_input('BMI value', min_value=0.0, max_value=100.0)
+    DiabetesPedigreeFunction = st.number_input('Diabetes Pedigree Function value', min_value=0.001, max_value=2.999,format="%.3f")
+    Age = st.number_input('Age of the Person', min_value=0, max_value=150, step=1)
     
     # Code for Prediction
     diagnosis = ''
@@ -42,9 +53,7 @@ def main():
         diagnosis = diabetes_prediction([Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age])
         
     st.success(diagnosis)
-    
-    
-    
+
 if __name__ == '__main__':
     main()
 
